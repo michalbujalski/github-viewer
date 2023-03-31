292887840ca1d5d97c3d08aebb4261787df52469
Michal Bujalski <michal@bujalski.dev>
2023-03-31 18:32:59 +0200

    Save sorting field in query params

--- src/components/RepositoriesTable.tsx
+++ src/components/RepositoriesTable.tsx
@@ -3,6 +3,31 @@ import { Repository } from '../models'
 import TableRow from './TableRow'
 import usePagination from '../hooks/usePagination'
 import Pagination from './Pagination'
+import useQuery from '../hooks/useQuery'
+import { ReactNode, useMemo } from 'react'
+
+const HeaderColumn = ({
+  children,
+  sortKey,
+  setSortBy,
+  slelected,
+}: {
+  children: ReactNode
+  slelected: boolean
+  sortKey: string
+  setSortBy: (key: string) => unknown
+}) => {
+  return (
+    <div className={`font-bold text-lg`}>
+      <button
+        className={`${slelected && 'underline'}`}
+        onClick={() => setSortBy(sortKey)}
+      >
+        {children}
+      </button>
+    </div>
+  )
+}
 
 const RepositoryTable = ({
   data,
@@ -11,12 +36,19 @@ const RepositoryTable = ({
   data: Repository[]
   className?: string
 }) => {
+  const { updateQuery, query } = useQuery()
+  const sortKey = useMemo(() => {
+    return query.get('sortBy')
+  }, [query.get('sortBy')])
+
   const { page, totalPages, setPage, paginatedData, setItemsPerPage } =
     usePagination<Repository>(0, 7, data)
-
+  const setSortBy = (key: string) => {
+    updateQuery([['sortBy', key]])
+  }
   return (
     <div className={`${className || ''} w-[600px]`}>
-      <div>
+      <div className="mb-4">
         <span>Items per page</span>{' '}
         <select
           onChange={(e) => {
@@ -28,14 +60,38 @@ const RepositoryTable = ({
           <option value={15}>15</option>
         </select>
       </div>
-
       {data.length > 0 ? (
         <>
+          <div>{sortKey}</div>
           <TableRow>
-            <div className="font-bold text-lg">Name</div>
-            <div className="font-bold text-lg">Owner</div>
-            <div className="font-bold text-lg">Stars</div>
-            <div className="font-bold text-lg">Created at</div>
+            <HeaderColumn
+              sortKey={'name'}
+              setSortBy={setSortBy}
+              slelected={sortKey === 'name'}
+            >
+              Name
+            </HeaderColumn>
+            <HeaderColumn
+              sortKey={'owner'}
+              setSortBy={setSortBy}
+              slelected={sortKey === 'owner'}
+            >
+              Owner
+            </HeaderColumn>
+            <HeaderColumn
+              sortKey={'stars'}
+              setSortBy={setSortBy}
+              slelected={sortKey === 'stars'}
+            >
+              Stars
+            </HeaderColumn>
+            <HeaderColumn
+              sortKey={'createdAt'}
+              setSortBy={setSortBy}
+              slelected={sortKey === 'createdAt'}
+            >
+              Created at
+            </HeaderColumn>
           </TableRow>
           {paginatedData.map((repo) => {
             return <RepositoryTableRow key={repo.id} data={repo} />
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { RepositoryProvider } from './context/repositories-provider'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RepositoryProvider>
-      <App />
+      <RouterProvider router={router} />
     </RepositoryProvider>
   </React.StrictMode>
 )
